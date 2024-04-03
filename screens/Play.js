024229a85d34f958775d1d01c3bdff3872750acb
Marina Tanaka <mtanakasg@gmail.com>
2024-04-03 11:05:45 -0300

    StopWatch

--- screens/Play.js
+++ screens/Play.js
@@ -1,4 +1,3 @@
-
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { useState, useRef } from "react";
 export default function Play() {
@@ -9,9 +8,13 @@ export default function Play() {
 
   // play
   const startStopwatch = () => {
-    startTimeRef.current = Date.now() - (time.hours * 3600 + time.minutes * 60 + time.seconds) * 1000;
+    startTimeRef.current =
+      Date.now() -
+      (time.hours * 3600 + time.minutes * 60 + time.seconds) * 1000;
     intervalRef.current = setInterval(() => {
-      const elapsedTime = Math.floor((Date.now() - startTimeRef.current) / 1000);
+      const elapsedTime = Math.floor(
+        (Date.now() - startTimeRef.current) / 1000
+      );
       const hours = Math.floor(elapsedTime / 3600);
       const minutes = Math.floor((elapsedTime % 3600) / 60);
       const seconds = elapsedTime % 60;
@@ -35,9 +38,13 @@ export default function Play() {
 
   /* Resume stopwatch */
   const resumeStopwatch = () => {
-    startTimeRef.current = Date.now() - (time.hours * 3600 + time.minutes * 60 + time.seconds) * 1000;
+    startTimeRef.current =
+      Date.now() -
+      (time.hours * 3600 + time.minutes * 60 + time.seconds) * 1000;
     intervalRef.current = setInterval(() => {
-      const elapsedTime = Math.floor((Date.now() - startTimeRef.current) / 1000);
+      const elapsedTime = Math.floor(
+        (Date.now() - startTimeRef.current) / 1000
+      );
       const hours = Math.floor(elapsedTime / 3600);
       const minutes = Math.floor((elapsedTime % 3600) / 60);
       const seconds = elapsedTime % 60;
@@ -46,94 +53,100 @@ export default function Play() {
     setRunning(true);
   };
   return (
-   
     <View style={styles.container}>
-    <Text style={styles.header}></Text>
-    <Text style={styles.subHeader}>Tempo</Text>
-    <Text style={styles.timeText} > {`${time.hours.toString().padStart(2, '0')}:${time.minutes.toString().padStart(2, '0')}:${time.seconds.toString().padStart(2, '0')}`}</Text>
-    <View style={styles.buttonContainer}>
-      {running ? (
-        <TouchableOpacity
-          style={[styles.button, styles.pauseButton]}
-          onPress={pauseStopwatch}
-        >
-          <Text style={styles.buttonText}>Pausar</Text>
-        </TouchableOpacity>
-      ) : (
-        <>
+      <Text style={styles.header}></Text>
+      <Text style={styles.subHeader}>Tempo</Text>
+      <Text style={styles.timeText}>
+        {" "}
+        {`${time.hours.toString().padStart(2, "0")}:${time.minutes
+          .toString()
+          .padStart(2, "0")}:${time.seconds.toString().padStart(2, "0")}`}
+      </Text>
+
+      <View style={styles.buttonContainer}>
+        {running ? (
           <TouchableOpacity
-            style={[styles.button, styles.startButton]}
-            onPress={startStopwatch}
+            style={[styles.button, styles.pauseButton]}
+            onPress={pauseStopwatch}
           >
-            <Text style={styles.buttonText}>Começar</Text>
+            <Text style={styles.buttonText}>Pausar</Text>
           </TouchableOpacity>
+        ) : (
+          <>
+            <TouchableOpacity
+              style={[styles.button, styles.startButton]}
+              onPress={startStopwatch}
+            >
+              <Text style={styles.buttonText}>Começar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.button, styles.resetButton]}
+              onPress={resetStopwatch}
+            >
+              <Text style={styles.buttonText}>Reset</Text>
+            </TouchableOpacity>
+          </>
+        )}
+        {!running && (
           <TouchableOpacity
-            style={[styles.button, styles.resetButton]}
-            onPress={resetStopwatch}
+            style={[styles.button, styles.resumeButton]}
+            onPress={resumeStopwatch}
           >
-            <Text style={styles.buttonText}>Reset</Text>
+            <Text style={styles.buttonText}>Retomar</Text>
           </TouchableOpacity>
-        </>
-      )}
-      {!running && (
-        <TouchableOpacity
-          style={[styles.button, styles.resumeButton]}
-          onPress={resumeStopwatch}
-        >
-          <Text style={styles.buttonText}>Retomar</Text>
-        </TouchableOpacity>
-      )}
+        )}
+      </View>
+
+      <View></View>
     </View>
-  </View>
-);
+  );
 }
 
 const styles = StyleSheet.create({
-container: {
-  flex: 1,
-  backgroundColor: "#fff",
-  alignItems: "center",
-  justifyContent: "center",
-},
-header: {
-  fontSize: 30,
-  color: "green",
-  marginBottom: 10,
-},
-subHeader: {
-  fontSize: 18,
-  marginBottom: 10,
-  color: "blue",
-},
-timeText: {
-  fontSize: 48,
-
-},
-buttonContainer: {
-  flexDirection: "row",
-  marginTop: 20,
-},
-button: {
-  paddingVertical: 10,
-  paddingHorizontal: 20,
-  borderRadius: 5,
-},
-startButton: {
-  backgroundColor: "#2ecc71",
-  marginRight: 10,
-},
-resetButton: {
-  backgroundColor: "#e74c3c",
-  marginRight: 10,
-},
-pauseButton: {
-  backgroundColor: "#f39c12",
-},
-resumeButton: {
-  backgroundColor: "#3498db",
-},
-buttonText: {
-  color: "white",
-  fontSize: 16,
-},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    fontSize: 30,
+    color: "green",
+    marginBottom: 10,
+  },
+  subHeader: {
+    fontSize: 18,
+    marginBottom: 10,
+    color: "blue",
+  },
+  timeText: {
+    fontSize: 48,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginTop: 20,
+  },
+  button: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  startButton: {
+    backgroundColor: "#2ecc71",
+    marginRight: 10,
+  },
+  resetButton: {
+    backgroundColor: "#e74c3c",
+    marginRight: 10,
+  },
+  pauseButton: {
+    backgroundColor: "#f39c12",
+  },
+  resumeButton: {
+    backgroundColor: "#3498db",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+  },
 });
