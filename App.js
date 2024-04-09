15d83982641c0b943d7ffe533afc13c035baa7a6
victor <victorhugomotabueno@gmail.com>
2024-04-09 10:42:40 -0300

    Concluido

--- App.js
+++ App.js
@@ -9,6 +9,11 @@ import Atividades from "./screens/Atividades";
 import Play from "./screens/Play";
 import Configuracao from "./screens/Configuracao";
 
+// Icones
+import { FontAwesome } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+
 const Tab = createBottomTabNavigator();
 
 export default function App() {
@@ -16,15 +21,61 @@ export default function App() {
     <>
       <StatusBar barStyle="light-content" />
       <NavigationContainer>
-        <Tab.Navigator 
-          screenOptions={{ headerShown: false }}
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+            tabBarLabelStyle: { fontSize: 16, padding: 5 },
+            tabBarStyle: styles.tabBar,
+          }}
           initialRouteName="Home"
         >
-          <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen name="Atividades" component={Atividades} />
-          <Tab.Screen name="Play" component={Play} />
-          <Tab.Screen name="Perfil" component={Perfil} />
-          <Tab.Screen name="Configuracao" component={Configuracao} />
+
+          <Tab.Screen
+            name="Inicio"
+            component={Home}
+            options={{
+              tabBarIcon: () => {
+                return <Ionicons name="home" size={24} color="white" />
+              }
+            }} />
+
+
+          <Tab.Screen
+            name="Histórico"
+            component={Atividades}
+            options={{
+              tabBarIcon: () => {
+                return <MaterialCommunityIcons name="clock-check" size={24} color="white" />
+              }
+            }} />
+
+          <Tab.Screen
+            name="Pedalar"
+            component={Play}
+            options={{
+              tabBarIcon: () => {
+                return <Ionicons name="bicycle-sharp" size={28} color="white" />
+              }
+            }} />
+
+          <Tab.Screen
+            name="Conta"
+            component={Perfil}
+            options={{
+              tabBarIcon: () => {
+                return <FontAwesome name="user" size={24} color="white" />
+              }
+            }} />
+
+          <Tab.Screen
+            name="Ajustes"
+            component={Configuracao}
+            options={{
+              tabBarIcon: () => {
+                return <Ionicons name="settings-sharp" size={24} color="white" />
+              }
+            }} />
+
         </Tab.Navigator>
       </NavigationContainer>
     </>
@@ -32,5 +83,10 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  
+  tabBar: {
+    backgroundColor: "#3D2498",
+    height: 80,
+  }
 });
+
+
