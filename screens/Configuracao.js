20e87b8ccfed414abb52cab55e79ea4d9535bfa4
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-04-12 11:03:09 -0300

    botão logout

--- screens/Configuracao.js
+++ screens/Configuracao.js
@@ -1,11 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
-
+import { Button, StyleSheet, Text, View } from "react-native";
+import { auth } from "../firebaseConfig";
+import { signOut } from "firebase/auth";
 export default function Configuracao() {
+  const logout = async () => {
+    try {
+      await signOut(auth);
+      navigation.replace("Inicial");
+    } catch (error) {
+      console.error(error.code);
+    }
+  };
   return (
     <View>
       <Text>Configuracao</Text>
+      <Button onPress={logout} title="Logout" color="#D35400" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
