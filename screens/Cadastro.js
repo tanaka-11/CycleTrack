055a3b2a945c7a07a5c9b638c2ef8dd7aa61d6bf
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-04-12 11:02:58 -0300

    Update Cadastro.js

--- screens/Cadastro.js
+++ screens/Cadastro.js
@@ -28,17 +28,17 @@ export default function Cadastro({ navigation }) {
 
   const [status, requestPermission] = ImagePicker.useCameraPermissions();
 
-  useEffect(() => {
-    async function verificaPermissoes() {
-      // CameraStatus guardando a requisição da permissão de camera
-      const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
+  // useEffect(() => {
+  //   async function verificaPermissoes() {
+  // CameraStatus guardando a requisição da permissão de camera
+  // const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
 
-      // Requisição da permissão recebendo o cameraStatus com o parametro de ter permitido
-      requestPermission(cameraStatus === "granted");
-    }
+  // Requisição da permissão recebendo o cameraStatus com o parametro de ter permitido
+  //     requestPermission(cameraStatus === "granted");
+  //   }
 
-    verificaPermissoes();
-  }, []);
+  //   verificaPermissoes();
+  // }, []);
 
   // Função para fazer upload da imagem para o Firebase Storage e depois enviar pela função Cadastrar
   const carregarStorage = async () => {
