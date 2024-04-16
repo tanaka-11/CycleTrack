c1529c2c84910c8c95b1b43a4b1248446093223f
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-04-16 16:07:06 -0300

    app.js e firebase

--- App.js
+++ App.js
@@ -25,22 +25,22 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   // Estado para rastrear o status de login do usuário
-  // const [isUserLoggedIn, setUserLoggedIn] = useState(false);
+  const [isUserLoggedIn, setUserLoggedIn] = useState(false);
 
-  // // Efeito para verificar o status de login do usuário ao montar o componente
-  // useEffect(() => {
-  //   const auth = getAuth(); // Obtém a instância de autenticação
-  //   const unsubscribe = onAuthStateChanged(auth, (user) => {
-  //     setUserLoggedIn(!!user); // Define isUserLoggedIn com base no estado de autenticação do usuário
-  //   });
+  // Efeito para verificar o status de login do usuário ao montar o componente
+  useEffect(() => {
+    const auth = getAuth(); // Obtém a instância de autenticação
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUserLoggedIn(!!user); // Define isUserLoggedIn com base no estado de autenticação do usuário
+    });
 
-  //   return unsubscribe; // Limpa a inscrição quando o componente for desmontado
-  // }, []);
+    return unsubscribe; // Limpa a inscrição quando o componente for desmontado
+  }, []);
 
   return (
     <SpeedProvider>
       <NavigationContainer>
-        {/* <Tab.Navigator initialRouteName={isUserLoggedIn ? "Home" : "Login"}> */}
+        {/* <Tab.Navigator */}
         <Tab.Navigator
           screenOptions={{
             headerShown: false,
@@ -50,33 +50,23 @@ export default function App() {
             tabBarActiveBackgroundColor: "#271177",
             tabBarStyle: styles.tabBar,
           }}
-          initialRouteName="Home"
+          initialRouteName={isUserLoggedIn ? "Home" : "Login"}
         >
-          {/* {isUserLoggedIn ? (
-          <>
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Atividades" component={Atividades} />
-            <Tab.Screen name="Play" component={Play} />
-            <Tab.Screen name="Perfil" component={Perfil} />
-            <Tab.Screen name="RecuperarSenha" component={RecuperarSenha} />
-            <Tab.Screen name="Configuracao" component={Configuracao} />
-          </>
-        ) : (
-          <>
-            <Tab.Screen name="Login" component={Login} />
-            <Tab.Screen name="Cadastro" component={Cadastro} />
-          </>
-        )} */}
-
-          <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen name="Atividades" component={Atividades} />
-          <Tab.Screen name="Play" component={Play} />
-          <Tab.Screen name="Perfil" component={Perfil} />
-          <Tab.Screen name="Configuracao" component={Configuracao} />
-
-          <Tab.Screen name="Login" component={Login} />
-          <Tab.Screen name="Cadastro" component={Cadastro} />
-          <Tab.Screen name="RecuperarSenha" component={RecuperarSenha} />
+          {isUserLoggedIn ? (
+            <>
+              <Tab.Screen name="Home" component={Home} />
+              <Tab.Screen name="Atividades" component={Atividades} />
+              <Tab.Screen name="Play" component={Play} />
+              <Tab.Screen name="Perfil" component={Perfil} />
+              <Tab.Screen name="Configuracao" component={Configuracao} />
+            </>
+          ) : (
+            <>
+              <Tab.Screen name="Login" component={Login} />
+              <Tab.Screen name="Cadastro" component={Cadastro} />
+              <Tab.Screen name="RecuperarSenha" component={RecuperarSenha} />
+            </>
+          )}
         </Tab.Navigator>
       </NavigationContainer>
     </SpeedProvider>
