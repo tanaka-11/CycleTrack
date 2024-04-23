488e66d168a9ea1d23e00202512d64ebdd2b0384
Marina Tanaka <huttenloucher@gmail.com>
2024-04-23 11:40:45 -0300

    estruturação inicial

---
+++ screens/Detalhes.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Detalhes() {
+  return (
+    <View>
+      <Text>Detalhes</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
