086d9a5d67b1e748d9e8e023dc6616235f605d3f
Marina Tanaka <huttenloucher@gmail.com>
2024-04-24 14:47:12 -0300

    Revert "Remove .firebaseConfig.js from Git tracking"
    This reverts commit 3c44711e3e2861aa315b97d63b609d2e75cba769.

---
+++ firebaseConfig.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCG-cLXqXjKZya_HY7fgsi6-jixLSMB1t4",
+  authDomain: "cycletrack-ts.firebaseapp.com",
+  projectId: "cycletrack-ts",
+  storageBucket: "cycletrack-ts.appspot.com",
+  messagingSenderId: "619328327967",
+  appId: "1:619328327967:web:0d0856de25bbe25003202a",
+};
+
+const app = initializeApp(firebaseConfig);
+
+// Initialize authentication with persistence
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+export { auth };
