0dac2417286b474a873f1457ee2d535af61e4034
Marina Tanaka <huttenloucher@gmail.com>
2024-04-30 17:21:16 -0300

    correção

--- components/SpeedContext.js
+++ components/SpeedContext.js
@@ -202,6 +202,10 @@ export const SpeedProvider = ({ children }) => {
     }
   };
 
+  // Constantes limite de velocidade e distancia minima
+  const MIN_SPEED = 0.5;
+  const MIN_CHANGE_IN_POSITION = 0.01;
+
   const updatePosition = (position) => {
     const currentSpeed = position.coords.speed || 0;
     setSpeed(currentSpeed);
@@ -213,21 +217,22 @@ export const SpeedProvider = ({ children }) => {
     setSpeedSum((prevSpeedSum) => prevSpeedSum + currentSpeed);
     setSpeedCount((prevSpeedCount) => prevSpeedCount + 1);
 
-    // Calcula a distância e atualiza o state 'distance'
-    if (myLocation) {
+    // Calcula a distância apenas se a velocidade for maior que o limite
+    if (myLocation && currentSpeed > MIN_SPEED) {
       const newDistance = calculateDistance(
         myLocation.coords.latitude,
         myLocation.coords.longitude,
         position.coords.latitude,
         position.coords.longitude
       );
-      setDistance((prevDistance) => prevDistance + newDistance);
+
+      // Atualiza o state distancia apenas for maior que o limite
+      if (newDistance > MIN_CHANGE_IN_POSITION) {
+        setDistance((prevDistance) => prevDistance + newDistance);
+      }
     }
 
-    // Adicione a localização atual ao array de localizações
-    setLocations((prevLocations) => [...prevLocations, position]);
-
-    // Atualiza a localização anterior com a localização atual
+    // Atualiza a localização atual
     setMyLocation(position);
   };
 
